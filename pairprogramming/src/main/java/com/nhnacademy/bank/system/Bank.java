76d838964e6a411eb48e18db821cd48d4581768d
이재영 <devempt@gmail.com>
2022-04-07 16:34:17 +0900

    .

--- pairprogramming/src/main/java/com/nhnacademy/bank/system/Bank.java
+++ pairprogramming/src/main/java/com/nhnacademy/bank/system/Bank.java
@@ -3,7 +3,7 @@ package com.nhnacademy.bank.system;
 import java.math.BigDecimal;
 
 public class Bank {
-    public Money exchange(Money money) throws NegativeException {
+    public static Money exchange(Money money) throws NegativeException {
         if (money.getCurrency().equals("dollar")) {
             BigDecimal exchangeAmt = money.getAmount().multiply(BigDecimal.valueOf(1_000)).setScale(0);
             if(exchangeAmt.remainder(BigDecimal.valueOf(10)).compareTo(BigDecimal.valueOf(5))==0){
