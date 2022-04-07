dbe140add33f5065a58096317cd6c1741c9fdd82
김세미 <semikimm.m@gmail.com>
2022-04-07 15:28:38 +0900

    1000원->1달러 환전

--- pairprogramming/src/main/java/com/nhnacademy/bank/system/Bank.java
+++ pairprogramming/src/main/java/com/nhnacademy/bank/system/Bank.java
@@ -1,4 +1,10 @@
 package com.nhnacademy.bank.system;
 
+import java.math.BigDecimal;
+
 public class Bank {
+    public Money exchange(Money thousandWon) throws NegativeException {
+        BigDecimal exchangeAmt = thousandWon.getAmount().divide(BigDecimal.valueOf(1_000));
+        return new Money(exchangeAmt,"dollar");
+    }
 }
--- pairprogramming/src/test/java/com/nhnacademy/bank/system/BankTest.java
+++ pairprogramming/src/test/java/com/nhnacademy/bank/system/BankTest.java
@@ -5,18 +5,43 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.math.BigDecimal;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BankTest {
     //SUT
     Bank bank;
     //DOC
-    Money money;
+    //Money money;
     @BeforeEach
     void setUp() {
-        bank = mock(Bank.class);
+        bank = new Bank();
+    }
+
+//    @DisplayName("은행을 통해서 환전할 수 있다.")
+//    @Test
+//    void exchange(){
+//        bank.exchange();
+//    }
+
+    @DisplayName("1,000원 -환전-> 1$")
+    @Test
+    void exchange_thousandWonToOneDollar() throws NegativeException {
+        Money thousandWon = new Money(BigDecimal.valueOf(1_000L),"won");
+        Money oneDollar = new Money(BigDecimal.valueOf(1),"dollar");
+
+        Money result = bank.exchange(thousandWon);
+        assertThat(result.equals(oneDollar)).isTrue();
+    }
+
+    @DisplayName("5.25$ -> 5,250원")
+    @Test
+    void exchange_decimalPointDollarToWon(){
+
+
     }
 
     @AfterEach
